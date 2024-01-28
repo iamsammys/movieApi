76818ab24b2e98d5fd602942643819b8a217d131
iamsammys <samuel.ezeh.246233@unn.edu.ng>
2024-01-28 11:28:52 +0100

    update added

--- models/movie.js
+++ models/movie.js
@@ -6,41 +6,62 @@
  */
 
 const { v4: uuidv4 } = require('uuid');
-const storage = require('../storage');
+const math = require('mathjs');
 
 
-class movie {
-    constructor(title, director, genre, releaseDate, rating, availableCopies) {
+class movieClass {
+    // movie class
+    constructor(dict=null, title=null, director=null, genre=null, releaseDate, rating=0, availableCopies=0, rented=false, maxAvailableCopies=10) {
+        if (dict && typeof dict === 'object') {
+            this._id = dict.id;
+            this._title = dict.title;
+            this._director = dict.director;
+            this._genre = dict.genre;
+            this._releaseDate = dict.releaseDate;
+            this._rating = dict.rating;
+            this._availableCopies = dict.availableCopies;
+            this._rented = dict.rented;
+            this._maxAvailableCopies = dict.maxAvailableCopies;
+        } else {
         this._id = uuidv4();
         this._title = title;
         this._director = director;
         this._genre = genre;
-        this._releaseDate = releaseDate;
+        this._releaseDate = new Date();
         this._rating = rating;
         this._availableCopies = availableCopies;
+        this._rented = rented;
+        this._maxAvailableCopies = maxAvailableCopies;
+        }
     }
 
     set setId(id) {
+        // set id
         this._id = id;
     }
 
     get getId() {
+        // get id
         return this._id;
     }
 
     set setTitle(title) {
+        // set title
         this._title = title;
     }
 
     get getTitle() {
+        // get title
         return this._title;
     }
 
     set setDirector(director) {
+        // set director
         this._director = director;
     }
 
     get getDirector() {
+        // get director
         return this._director;
     }
 
@@ -49,34 +70,52 @@ class movie {
     }
 
     get getGenre() {
+        // get genre
         return this._genre;
     }
 
     set setReleaseDate(releaseDate) {
+        // set release date
         this._releaseDate = releaseDate;
     }
 
     get getReleaseDate() {
+        // get release date
         return this._releaseDate;
     }
 
     set setRating(rating) {
+        // set rating
         this._rating = rating;
     }
 
     get getRating() {
+        // get rating
         return this._rating;
     }
 
     set setAvailableCopies(availableCopies) {
-        this._availableCopies = availableCopies;
+        // set available copies
+        this._availableCopies = math.min(availableCopies, this._maxAvailableCopies);
     }
 
     get getAvailableCopies() {
+        // get available copies
         return this._availableCopies;
     }
 
+    set setMaxAvailableCopies(maxAvailableCopies) {
+        // set max available copies
+        this._maxAvailableCopies = maxAvailableCopies;
+    }
+
+    get getMaxAvailableCopies() {
+        // get max available copies
+        return this._maxAvailableCopies;
+    }
+
     toDict() {
+        // return movie as an object
         return {
             id: this._id,
             title: this._title,
@@ -84,7 +123,59 @@ class movie {
             genre: this._genre,
             releaseDate: this._releaseDate,
             rating: this._rating,
-            availableCopies: this._availableCopies
+            availableCopies: this._availableCopies,
+            maxAvailableCopies: this._maxAvailableCopies,
+            rented: this._rented
         }
     }
+
+    rentMovie() {
+        // rent movie
+        if (this._availableCopies > 0) {
+            this._availableCopies -= 1;
+            this._rented = true;
+            this.save()
+            return true;
+        }
+        return false;
+    }
+
+    returnMovie() {
+        // return movie
+        if (this._rented) {
+            if (this._availableCopies < this._maxAvailableCopies) {
+                this._availableCopies += 1;
+                if (this._availableCopies === this._maxAvailableCopies) {
+                    this._rented = false;
+                }
+                this.save()
+                return true
+            } else {
+                return 'max copies reached';
+            }
+        } else {
+            return 'movie not rented or max copies reached';
+        }
+    }
+
+    update() {
+        // update movie
+        const storage = require('../storage/movieStorage');
+        this.setReleaseDate = new Date();
+        this.save();
+    }
+
+    save() {
+        // save movie
+        const storage = require('../storage/movieStorage');
+        storage.addMovie(this);
+        storage.saveMovie();
+    }
+
+    delete() {
+        // delete movie
+        const storage = require('../storage/movieStorage');
+        storage.deleteMovie(this);
+    }
 }
+module.exports = movieClass;
--- storage/movieStorage.js
+++ storage/movieStorage.js
@@ -1,15 +1,105 @@
 #!/usr/bin/env node
 /**
  * module to implement the movie storage
+ * created by:
+ *       Samuel Ezeh
  */
 
+const fs = require('fs');
 movies = {};
-fileName = "movies.json";
+fileName = "./movies.json";
 
 
 class movieStorage {
-    addMovie(movie) {
+    static addMovie(movie) {
         // add movie to storage
-        key = ``
+        const newKey = `${movie.getId}.${movie.getTitle}`;
+        movies[newKey] = movie;
+        for (const key in movies) {
+            const similarMovie = movies[key];
+            if (similarMovie.getId === movie.getId) {
+                if (newKey !== key) {
+                    this.deleteMovie(null, key);
+                }
+            }
+        }
+    }
+
+    static getAllMovies(movieTitle=null) {
+        // return all movies in storage
+        if (movieTitle) {
+            for (const key in movies) {
+                const movie = movies[key];
+                if (movie.getTitle == movieTitle)
+                    return movie;
+            }
+        }
+        return movies;
+    }
+
+    static rentMovie(movieTitle) {
+        // rent movie from storage
+        for (const key in this.getAllMovies()) {
+            const movie = movies[key];
+            if (movie.getTitle == movieTitle)
+                return movie.rentMovie();
+        }
+        return 'Movie not found'
+    }
+    
+    static returnMovie(movieTitle) {
+        // return movie to storage
+        for (const key in movies) {
+            const movie = movies[key];
+            if (movie.getTitle == movieTitle) {
+                return movie.returnMovie();
+            } else {
+                return 'Movie not found'
+            }
+        }
+    }
+
+    static saveMovie() {
+        // save movies to storage
+        const newDict = {};
+        for (const key in movies) {
+            const obj = movies[key];
+            newDict[key] = obj.toDict();
+        }
+        const json = JSON.stringify(newDict);
+        fs.writeFileSync(fileName, json, 'utf8', (err) => {
+            if (err) {
+                console.log(err);
+            }
+        });
+    }
+
+    static reload() {
+        // reload movies from storage
+        try {
+            const movieClass = require('../models/movie');
+            const data = fs.readFileSync(fileName, 'utf8');
+            const obj = JSON.parse(data);
+
+            for (const key in obj) {
+                const movieDict = obj[key];
+                const newMovie = new movieClass(movieDict);
+                movies[key] = newMovie;
+            }
+        }
+        catch (err) {}
+    }
+
+    static deleteMovie(movie,key=null) {
+        // delete movie from storage
+        if (key) {
+            delete movies[key];
+        } else {
+            const key = `${movie.getId}.${movie.getTitle}`;
+            delete movies[key];
+        this.saveMovie();
+        }
     }
 }
+movieStorage.reload();
+module.exports = movieStorage;
